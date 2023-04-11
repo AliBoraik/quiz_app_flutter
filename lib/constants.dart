e25f948da6e0ea4a537ccc278080bf9fb460d757
Ali <ali99aqel@gmail.com>
2023-04-11 23:59:05 +0300

    add constants

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kSecondaryColor = Color(0xFF1A2330);
+const kGreenColor = Color(0xFF44AF29);
+const kRedColor = Color(0xFFE92E30);
+const kGrayColor = Color(0xFFC1C1C1);
+const kBlackColor = Color(0xFF101010);
+const kDividerColor = Color.fromARGB(255, 69, 69, 69);
+const kPrimaryGradient = LinearGradient(
+  colors: [Color(0xFF46A0AE), Color(0xFF00FFCB)],
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+);
+
+const double kDefaultPadding = 20.0;
+// string constants
+const kQuizAppTitle = "Quiz App";
